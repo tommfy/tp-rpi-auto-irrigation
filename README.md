af72f18c6abf9e4a7294b061fef2c1b69283ce3c
Tom Pollock <tapollock@outlook.com>
2021-06-17 13:47:58 +0100

    Updates to code

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 Code to go along with the [Raspberry Pi Controlled Irrigation System](http://www.instructables.com/id/Raspberry-Pi-Controlled-Irrigation-System) Instructable by Ben Finio.
 
-This code contains the script "run_sprinkler.py" which automatically runs an outside sprinkler 
-on a fixed schedule using a crontab.  The sprinkler will only run if there has been little to 
-no rain in the past 24 hours.  The script uses the Weather Underground API to estimate recent rainfall
+I have created my own Open Weather code because I'm too cheap to get the Wunderground API key :D
 
-All credit goes to this guy for creating everything:
-https://github.com/markveillette/rpi_sprinkler
-
-I just tailored it to my settings.
+This code contains the script "tp_irrigate.py" which automatically runs an outside sprinkler/drip feeder
+on a fixed schedule using a crontab. The sprinkler will only run if the current forecast is not rain.  The script uses the Open Weather API to get current rainfall
---
+++ tp_irrigate.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import requests
+import ConfigParser
+import datetime
+from time import sleep
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+# importing requests and json
+import requests, json
+# base URL
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat=51.37610581678528&lon=0.44608559709327733&appid=XXXXXXXXXXXXXX"
+# HTTP request
+response = requests.get(BASE_URL)
+
+
+# OR USE CITY
+#BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+#City Name CITY = "London"
+#API key API_KEY = "Your API Key"
+#URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+
+
+# Checking the status code of the request
+# Test API access
+if response.status_code == 200:
+
+   # getting data in the json format
+   data = response.json()
+   # weather report
+   report = data['weather']
+   print(f"Weather Report: {report[0]['description']}")
+
+else:
+   # showing the error message
+   print("Error connecting to API")
+
+
+
+  # If rain then run sprinkler
+  if report not in ['rain', 'shower rain', 'thunderstorm', 'snow', 'Rain', 'Shower Rain', 'Thunderstorm', 'Snow', 'Shower rain', 'Thunder storm', 'Thunder Storm']:
+    run_sprinkler(config)
